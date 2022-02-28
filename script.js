01bca5d57a505f30eb51af2ad2be3a70ddfdc0f7
Akshar Bisht <bisht.akshar@gmail.com>
2022-02-28 15:30:42 +0530

    added function match() to compare values

--- script.js
+++ script.js
@@ -2,5 +2,38 @@ function computerPlay(choices) {
   return choices[Math.floor(Math.random() * 3)];
 }
 
+function match(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLower();
+  computerSelection = computerSelection.toLower();
+
+  if (playerSelection === "rock") {
+    if (computerSelection === "paper") {
+      return "You Lose! Paper beats rock";
+    } else if (computerSelection === "scissors") {
+      return "You win! Rock beats scissors";
+    } else {
+      return "It's a tie! You both chose Rock";
+    }
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      return "You win! Paper beats Rock";
+    } else if (computerSelection === "scissors") {
+      return "You lose! Scissors beat Paper";
+    } else {
+      return "It's a tie! You both chose Paper";
+    }
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "rock") {
+      return "You lose! Rock beats Scissors";
+    } else if (computerSelection === "paper") {
+      return "You win! Scissors beat Paper";
+    } else {
+      return "It's a tie! You both chose scissors";
+    }
+  } else {
+    return "Invalid choice!";
+  }
+}
+
 let choices = ["Rock", "Paper", "Scissors"];
-console.log(computerPlay(choices));
+// console.log(computerPlay(choices));
